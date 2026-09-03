7e210b05bf82589c2aef5c1249665eda50798ae9
Laksh0511 <kulshresthalakshya3@gmail.com>
2026-09-04 00:36:35 +0530

    implemented computer choice function and verified it

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+function getComputerChoice(){
+    let r= Math.floor(Math.random()*3);//random from 0,1,2
+
+    if(r==0){
+        return "Rock";
+    }
+    else if(r===1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+
+console.log(getComputerChoice());
